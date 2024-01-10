364f354ea778feeea6586569a11ecb85a3acb026
Mukesh Bartaula <mukesh.bartaula109@gmail.com>
2024-01-10 20:58:59 +0545

    implement edit function

--- src/component/TodoForm.jsx
+++ src/component/TodoForm.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function TodoForm({ data, setData }) {
     const [inputTitleValue, setinputTitleValue] = useState('');
     const [inputDescValue, setinputDescValue] = useState('');
-    const [error, setError] = useState('')
+
 
     const handleTitleInput = (e) => {
         setinputTitleValue(e.target.value)
@@ -12,33 +12,27 @@ function TodoForm({ data, setData }) {
     const handleDescInput = (e) => {
         setinputDescValue(e.target.value)
     }
-    const handleData = () => {
-        if (inputTitleValue === '' || inputDescValue === '') {
-            setError("Both of the field is required")
-            return false
-        }
-        setError('')
-        setData([...data, { inputTitleValue: inputTitleValue, inputDescValue: inputDescValue }]);
-        setinputTitleValue('');
-        setinputDescValue('');
+    const handleData = (e) => {
+        e.preventDefault();
+        setData([...data, { inputTitleValue: inputTitleValue, inputDescValue: inputDescValue }])
+        setinputTitleValue('')
+        setinputDescValue('')
 
     }
 
     return (
         <>
-            <form>
-                <input className="input px-4 my-2 w-100" type="text" value={inputTitleValue} placeholder="Enter Title" onChange={handleTitleInput} />
+            <form className="mb-4" onSubmit={(e) => handleData(e)} >
+                <input className="input px-4 my-2 w-100" type="text" value={inputTitleValue} placeholder="Enter Title" onChange={handleTitleInput} required />
                 <br />
 
-                <input className="input px-4 my-2 w-100" type="text" value={inputDescValue} placeholder="Enter Desccription" onChange={handleDescInput} />
+                <input className="input px-4 my-2 w-100" type="text" value={inputDescValue} placeholder="Enter Desccription" onChange={handleDescInput} required />
                 <br />
 
                 <div className="text-start">
-                    <button type="button" className="btn btn-dark my-1" onClick={handleData} >Add Todo</button>
+                    <button type="submit" className="btn btn-primary my-1"  >Add Todo</button>
                 </div>
-                <div>
-                    {error}
-                </div>
+
             </form>
         </>
     )
--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,34 +1,107 @@
+import React, { useState } from "react"
 function TodoList({ data, setData }) {
+
+    const [updateTitle, setupdateTitle] = useState('')
+    const [updateDesc, setupdateDesc] = useState('')
+
+    const [id, setoldTitle] = useState('')
+
+
+    const handleUpdateTitle = (Title) => {
+        console.log(Title)
+        setupdateTitle(Title)
+    }
+    const handleUpdateDesc = (desc) => {
+        console.log(desc)
+        setupdateDesc(desc)
+    }
+    const handleEdit = (e, id) => {
+        e.preventDefault();
+        const newData = [...data]
+        data.map((item, index) => {
+            if (index == id) {
+                const updatedData = {
+                    inputTitleValue: updateTitle,
+                    inputDescValue: updateDesc
+                }
+                newData[id] = updatedData
+            }
+        });
+        setData(newData)
+    }
+    const checkingData = (e, id) => {
+
+        setupdateTitle(title)
+
+    }
     const deleteData = (id) => {
         const newList = data.filter((_, index) => index !== id)
         setData(newList)
     }
-
     return (
         <>
-            <table className=" table table-striped w-100">
-                <thead>
-                    <tr>
-                        <th>SN</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th colSpan="2">Operation</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => (
+            <div className="container">
+                <table className=" table  table-hover border  ">
+                    <thead>
                         <tr>
-                            <td>{index + 1}</td>
-                            <td>{item.inputTitleValue}</td>
-                            <td>{item.inputDescValue}</td>
-                            <td><button type="button" className="btn btn-danger" onClick={() => deleteData(index)}>Delete</button></td>
+                            <th>SN</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th colSpan="3" style={{ width: '150px' }} >Operation</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
 
-                    ))}
+                            < tr key={index} >
+                                <td>{index + 1}</td>
+                                <td>{item.inputTitleValue}</td>
+                                <td>{item.inputDescValue}</td>
+                                <td >
+                                    <div style={{ display: 'flex', height: '25px', width: '20px', cursor: 'pointer' }}>
+                                        <img src="./completed.png" onClick={(e) => completeTask(index)} />
+                                    </div>
+                                </td>
+                                <td >
+                                    <div style={{ display: 'flex', height: '25px', width: '20px', cursor: 'pointer' }}>
+                                        <img src="./edit.png" data-bs-toggle="modal" data-bs-target={`#editForm${index}`} />
+                                        <div className="modal fade" id={`editForm${index}`}>
+                                            <div className="modal-dialog">
+                                                <div className="modal-content">
+                                                    <div className="modal-header">
+                                                        <h2 className="modal-title" >Edit Form</h2>
+                                                        <button type="button" className="btn-close" data-bs-dismiss="modal" ></button>
+                                                    </div>
+                                                    <div className="modal-body">
+                                                        <form onSubmit={(e) => handleEdit(e, index)}>
+                                                            <div className="mb-3">
+                                                                <input type="text" className="form-control" value={updateTitle.inputTitleValue} onChange={(e) => handleUpdateTitle(e.target.value)} />
+                                                            </div>
+                                                            <div className="mb-3">
+                                                                <input type="text" className="form-control" value={updateTitle.inputDescValue} onChange={(e) => handleUpdateDesc(e.target.value)} />
+                                                            </div>
+                                                            <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Update</button>
+                                                        </form>
+                                                    </div>
 
-                </tbody>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td >
+                                    <div style={{ display: 'flex', height: '25px', width: '20px', cursor: 'pointer' }}>
+                                        <img src="./delete.png" onClick={() => deleteData(index)} />
+                                    </div>
+                                </td>
+                            </tr>
 
-            </table>
+                        ))}
+
+                    </tbody>
+
+                </table >
+            </div >
         </>
     )
 }
